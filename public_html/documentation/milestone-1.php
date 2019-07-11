60207515a667931b5547ff93ac03cfeb6f7e404a
Marsha Battee <marsha@sanesuite.com>
2019-07-11 17:21:51 -0600

    milestone 1 structure created

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8"/ >
+		<title>PWP-Marsha-Battee</title>
+
+	</head>
+
+	<body>
+
+		<div>
+			<h1>Personal Website Project - Marsha Battee</h1>
+			<h2>Milestone 1</h2>
+		</div>
+
+		<div>
+			<h3>Purpose, Audience and Goal</h3>
+			<p>Purpose</p>
+			<p>Audience<p>
+			<p>Goal</p>
+		</div>
+
+		<div>
+			<h3>Persona</h3>
+			<p>Demographics</p>
+			<p>Goals<p>
+			<p>Fashion Habits</p>
+			<p>Tech</p>
+			<p>Frustrations</p>
+		</div>
+
+
+
+	</body>
+</html>
