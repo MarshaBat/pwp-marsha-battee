16d5f0793820e0ff0ba0fca42ff37c97c59b6347
Marsha Battee <marsha@sanesuite.com>
2019-07-11 21:01:12 -0600

    completed purpose, audience and goals of milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,13 +2,11 @@
 <html lang="en">
 
 	<head>
-		<meta charset="UTF-8"/ >
+		<meta charset="UTF-8"/>
 		<title>PWP-Marsha-Battee</title>
-
 	</head>
 
 	<body>
-
 		<div>
 			<h1>Personal Website Project - Marsha Battee</h1>
 			<h2>Milestone 1</h2>
@@ -16,20 +14,24 @@
 
 		<div>
 			<h3>Purpose, Audience and Goal</h3>
-			<p>Purpose</p>
-			<p>Audience<p>
-			<p>Goal</p>
+			<p>Our Purpose - We design and develop bold, innovative technology solutions to help survivor support agencies
+				reach out and engage with their clients during their reporting and healing process.</p>
+			<p>Our Audience - Our audience is composed of stakeholders and key technology and financial decision makers in
+				forensic programs and medical, legal, and support agencies that care for survivors of sexual and domestic
+				violence.
+			<p>
+			<p>Our Goal - Our website is positioned to attract clients who need website design and development in the above stated agencies.</p>
 		</div>
 
 		<div>
 			<h3>Persona</h3>
 			<p>Demographics</p>
-			<p>Goals<p>
+			<p>Goals
+			<p>
 			<p>Fashion Habits</p>
 			<p>Tech</p>
 			<p>Frustrations</p>
 		</div>
-
 
 
 	</body>
