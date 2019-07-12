64e2f55fb2a0a2da30caa6490fb22040f5ddaef9
Marsha Battee <marsha@sanesuite.com>
2019-07-11 22:07:46 -0600

    added content for persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,28 +9,67 @@
 	<body>
 		<div>
 			<h1>Personal Website Project - Marsha Battee</h1>
-			<h2>Milestone 1</h2>
+			<h2>MILESTONE 1</h2>
 		</div>
 
 		<div>
-			<h3>Purpose, Audience and Goal</h3>
-			<p>Our Purpose - We design and develop bold, innovative technology solutions to help survivor support agencies
+			<h3>PURPORSE, AUDIENCE and GOAL</h3>
+			<p><strong>Our Purpose</strong> - We design and develop bold, innovative technology solutions to help survivor support agencies
 				reach out and engage with their clients during their reporting and healing process.</p>
-			<p>Our Audience - Our audience is composed of stakeholders and key technology and financial decision makers in
+			<p><strong>Our Audience</strong> - Our audience is composed of stakeholders and key technology and financial decision makers in
 				forensic programs and medical, legal, and support agencies that care for survivors of sexual and domestic
 				violence.
 			<p>
-			<p>Our Goal - Our website is positioned to attract clients who need website design and development in the above stated agencies.</p>
+			<p><strong>Our Goal</strong> - Our website is positioned to attract clients who need website design and development in the above stated agencies.</p>
 		</div>
 
 		<div>
-			<h3>Persona</h3>
-			<p>Demographics</p>
-			<p>Goals
-			<p>
-			<p>Fashion Habits</p>
-			<p>Tech</p>
-			<p>Frustrations</p>
+			<h3>SAMPLE PERSONA</h3>
+			<p><strong>Demographics - Linda Collins, DNP, MSN, BSN, WHNP, SANE-A, SANE-P, CCRN</strong></p>
+				<ul>
+					<li>Age: 47</li>
+					<li>Location: Fairfax, VA</li>
+					<li>Education: Graduate of VA schools and universities</li>
+					<li>Job: Executive Director of a Fairfax Medical Forensic Nursing Providers</li>
+					<li>Family: Married with 3 children (male, 2 in college and 1 is a nurse)</li>
+				</ul>
+			<p><strong>About</strong></p>
+			<p>Linda has been the Executive Director of Fairfax Medical Forensic Providers (FMFP) for 12 years. Her career
+				in nursing began on a Mother/Baby unit in 1998. Since that time, Linda has worked in labor and delivery,
+				emergency departments, earned advanced degrees, and worked as a Women’s Health Nurse Practitioner (WHNP). In
+				2009, Linda found her calling working as a forensic nurse and became a board-certified Sexual Assault Nurse
+				Examiner for Adults and Pediatrics (SANE-A and SANE-P, respectively).</p>
+			<p>Just 2 years ago, Linda was promoted to the role of Executive Director in her organization. And since her
+				time at FMFP, her organization has had a limited online presence with a website that was built around the
+				time that Linda became an employee.</p>
+			<p>Linda is seeking to update FMFP’s web presence and create a user friendly site, highlighting resources in
+				the community for users.</p>
+			<p><strong>Goal</strong></p>
+				<ul>
+					<li>Update her organization’s web presence in order to support more survivors of sexual and domestic violence.</li>
+				</ul>
+			<p><strong>Use of Site</strong></p>
+				<ul>
+					<li>To provide information on services provided FMFP.</li>
+					<li>To connect survivors with reporting and healing resources for survivors.</li>
+					<li>To keep stakeholders up-to-date on FMFP’s initiatives and survivor services.</li>
+				</ul>
+			<p><strong>Tech</strong></p>
+				<ul>
+					<li>Savviness: Excellent.</li>
+					<li>Social Network: Facebook for friends and family.</li>
+				</ul>
+			<p><strong>Frustrations</strong></p>
+				<ul>
+					<li>Linda is trying to find the money in her budget to pay for website design and development.</li>
+					<li>Linda has to convince FMFP board members that investing in a new website could help them better
+						support the survivor community.
+					</li>
+					<li>Linda’s staff is happy with word of mouth and believe that fliers, business cards and a better
+						community presence through workshops and other events will keep survivors in the know. They feel that
+						there’s no need for the expense.
+					</li>
+				</ul>
 		</div>
 
 
