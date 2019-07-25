6325825fc0f9d97e39aa962b85013d343e019fde
Marsha Battee <marsha@sanesuite.com>
2019-07-24 21:45:52 -0600

    initial draft of content strategy added to milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8"/>
+		<title>PWP-Marsha-Battee</title>
+	</head>
+
+	<body>
+		<div>
+			<h1>Personal Website Project - Marsha Battee</h1>
+			<h2>MILESTONE 2</h2>
+		</div>
+
+		<div>
+		<h3>CONTENT STRATEGY</h3>
+
+		<h4>Landing Page (Home)</h4>
+		<p>The home page will feature the company logo and an image slider, highlighting photos of survivors. Over the
+			slider, there will be a welcome message (e.g., a working message-- “we help you better CARE for SURVIVORS”).
+			Below
+			that message will be a “learn more” button. Navigation will feature a hamburger menu that links to the various
+			sections (listed below) of this one-paged website.</p>
+			<ul>
+				<li>Inspiration: UNHCR, Apple Watch, RAINN</li>
+			</ul>
+
+		<h4>About</h4>
+		<p>This section, on a nondescript background, will include our golden circle-- a brief statement on our why, what
+			and how. Also stated will be our mission and vision for our work and in our working with clients. Likely a
+			white or light-colored background to make the text stand out.</p>
+			<ul>
+				<li>Inspiration: _______</li>
+			</ul>
+
+		<h4>Our Services</h4>
+		<p>This section will list 3-6________________</p>
+			<ul>
+				<li>Inspiration: RAINN</li>
+			</ul>
+
+		<h4>Our Process</h4>
+		<p>This section will highlight our development process utilizing vector images with limited text descriptors.
+			These vector images and text will be on a background image that highlights portions of the design process,
+			including behind-the-scene photos and sketches.</p>
+			<ul>
+				<li>Inspiration: https://www.canva.com/learn/portfolio/</li>
+			</ul>
+
+		<h4>Portfolio</h4>
+		<p>This section will showcase, in a clean and simple gallery format, 3 examples of my development work with links
+			to those respective websites. This section will also identify the technology, process and context used on each
+			project.</p>
+			<ul>
+				<li>Inspiration: __________________________</li>
+			</ul>
+
+		<h4>Contact</h4>
+		<p>The contact information will be housed in the footer. This section will contain the required contact form where
+			visitors can submit their name, subject and message to send or reset. This section will also include a mailto
+			link to my business e-mail address and icon links to any associated business-related social media sites.
+			Visitors will also see the business tag-line or a brief one-line description of our mission, our business name,
+			address, and phone number.</p>
+			<ul>
+				<li>Inspiration: ___________________________</li>
+			</ul>
+		</div>
+
+		<div>
+		<h3>WIREFRAMES</h3>
+
+		</div>
+
+
+	</body>
+</html>
