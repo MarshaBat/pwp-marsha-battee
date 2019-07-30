833d4dcdfd02ee04f8afa0ff17bc5144a71da508
Marsha Battee <marsha@sanesuite.com>
2019-07-29 20:25:23 -0600

    revised content to home, about, services, process; and added example images to those sections

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,19 +9,22 @@
 	<body>
 		<div>
 			<h1>Personal Website Project - Marsha Battee</h1>
-			<h2>MILESTONE 2</h2>
+			<h2>MILESTONE 2A</h2>
 		</div>
 
 		<div>
 		<h3>CONTENT STRATEGY</h3>
 
 		<h4>Landing Page (Home)</h4>
-			<p>The home page will feature the company logo and an image slider, highlighting photos of survivors. Over the
-				slider, there will be a welcome message (e.g., --working message-- “we help you better CARE for SURVIVORS”).
-				Below that message will be a “learn more” button. Navigation will feature a hamburger menu that links to the
-				various sections (listed below) of this one-paged website.</p>
+			<p>The home page will feature the company logo and a banner image, highlighting a photo of a survivor. Over the
+				image, there will be a welcome message including flashing keywords (e.g., --working message-- “we help you
+				'better care for' ('communicate your message to' or 'support') survivors”. Below that message will be a “learn
+				more” button. Navigation will feature a hamburger menu that links to the various sections (listed below) of
+				this one-paged website.</p>
 			<ul>
-				<li>Inspiration: UNHCR, Apple Watch, RAINN</li>
+				<li><a href="https://www.unhcr.org/fr-fr/monpaysestuncamp/">Inspiration: UNHCR Home Page</a></li>
+				<br>
+				<img src="bannerimage1.jpg" alt="banner example at uhncr.org">
 			</ul>
 
 		<h4>About</h4>
@@ -29,13 +32,17 @@
 				what and how. Also stated will be our mission and vision for our work and in our working with clients.
 				Likely a white or light-colored background to make the text stand out.</p>
 			<ul>
-				<li>Inspiration: _______</li>
+				<li><a href="https://thebossynurse.com/">Example: About Section</a></li>
+				<br>
+				<img src="aboutsection1.jpg" alt="about section example">
 			</ul>
 
 		<h4>Our Services</h4>
-		<p>This section will list 3-6 of our services in a color block format (at least 3, max 6).</p>
+			<p>This section will list 3-6 of our services in a color block format (at least 3, max 6).</p>
 			<ul>
-				<li>Inspiration: RAINN</li>
+				<li><a href="https://rainn.org">Inspiration: RAINN</a></li>
+				<br>
+				<img src="colorblock1.jpg" alt="color block image from rainn.org">
 			</ul>
 
 		<h4>Our Process</h4>
@@ -43,30 +50,30 @@
 				These vector images and text will be on a background image that highlights portions of the design process,
 				including behind-the-scene photos, sketches, or wireframes, etc.</p>
 			<ul>
-				<li>Inspiration: https://www.canva.com/learn/portfolio/</li>
+				<li><a href="https://www.apple.com/apple-watch-series-4/">Example: Vector Images</a></li>
+				<br>
+				<img src="vectorimage1.jpg" alt="vector image from apple.com">
 			</ul>
 
 		<h4>Portfolio</h4>
 			<p>This section will showcase, in a clean and simple gallery format, 3 examples of my design/development work
-				with links to those respective websites. This section will also identify the technology, process and context
+				with links to those respective websites. As you hoover over each block, either a color change or animation
+				to the block occurs. This section will also identify the technology, process and context
 				used on each project-- and case study if applicable.</p>
 			<ul>
 				<li>Inspiration: __________________________</li>
 			</ul>
 
 		<h4>Contact</h4>
-			<p>The contact information will be housed in the footer. This section will contain the required contact form
-				where visitors can submit their name, subject and message to send or reset. This section will also include a
-				mailto link to my business e-mail address and icon links to any business-related social media sites.
-				Visitors will also see the business tag-line or a brief one-line description of our mission, our business
-				name, address, and phone number.</p>
-			<ul>
-				<li>Inspiration: ___________________________</li>
-			</ul>
+			<p>The contact section will contain the required contact form where visitors can submit their name, subject and
+				message to submit. This section will also include a mailto link to my business e-mail address and icon links
+				to any business-related social media sites. Visitors will also see the business tag-line or a brief one-line
+				description of our mission, our business name, address, and phone number.</p>
 		</div>
 
 		<div>
 		<h3>WIREFRAMES</h3>
+			<img src="marsha-pwp-wireframe-072919.png" alt="pwp wireframe - marsha battee">
 
 		</div>
 
