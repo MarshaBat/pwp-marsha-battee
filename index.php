f53bb415d6f1882bed267b2c1a23baa7945429f7
Marsha Battee <marsha@sanesuite.com>
2019-08-06 19:08:06 -0600

    created navbar and hamburger menu

--- index.php
+++ index.php
@@ -7,25 +7,37 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 		<!-- My Stylesheet -->
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<title>Working Title for PWP</title>
+
 	</head>
 	<body>
-
-
-
-
-
-
-
-
-
-
-
+		<!------------------------------------------------SECTION 1 - HEADER---------------------------------------------->
+		<div class="container-fluid">
+			<nav class="navbar navbar-expand-xl bg-transparent navbar-dark rounded-lg m-4">
+				<a class="navbar-brand text-secondary" href="#">Our Working Title</a>
+				<button class="navbar-toggler border bg-secondary" type="button" data-toggle="collapse"
+						  data-target="#collapsibleNavbar">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+						<li class="nav-item"><a class="nav-link text-secondary px-3" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link text-secondary px-3" href="#">About</a></li>
+						<li class="nav-item"><a class="nav-link text-secondary px-3" href="#">Our Services</a></li>
+						<li class="nav-item"><a class="nav-link text-secondary px-3" href="#">Our Process</a></li>
+						<li class="nav-item"><a class="nav-link text-secondary px-3" href="#">Contact</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 
 
 	</body>
 </html>
+
+public_html/documentation/bannerimage1.jpg
