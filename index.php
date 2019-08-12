2996135c6c3d148530869b4a15aab48adbcdc466
Marsha Battee <marsha@sanesuite.com>
2019-08-11 21:40:19 -0600

    added contact form

--- index.php
+++ index.php
@@ -31,13 +31,14 @@
 							<li class="nav-item"><a class="nav-link text-white px-3" href="#">About</a></li>
 							<li class="nav-item"><a class="nav-link text-white px-3" href="#">Our Services</a></li>
 							<li class="nav-item"><a class="nav-link text-white px-3" href="#">Our Process</a></li>
+							<li class="nav-item"><a class="nav-link text-white px-3" href="#">Portfolio</a></li>
 							<li class="nav-item"><a class="nav-link text-white px-3" href="#">Contact</a></li>
 						</ul>
 					</div>
 				</nav>
 			</div>
 		</section>
-		<!------------------------------------------------SECTION 2 - ABOUT---------------------------------------------->
+		<!------------------------------------------------SECTION 1 - HOME----------------------------------------------->
 		<section>
 			<div class="jumbotron jumbotron-fluid img-fluid">
 				<div class="banner-overlay"></div>
@@ -48,80 +49,88 @@
 						<div class="col banner-title">Our Working Title</div>
 					</div>
 
-						<div class="row banner-text">
-							<div class="col banner-tag">We help you better care for survivors</div>
+					<div class="row banner-text">
+						<div class="col banner-tag">We help you better care for survivors</div>
+					</div>
+
+				</div>
+			</div>
+			</div>
+		</section>
+
+
+		<!------------------------------------------------SECTION 2 - ABOUT---------------------------------------------->
+		<section>
+			<div class="about-background">
+				<div class="container mb-5">
+					<div class="row">
+						<div class="col-sm-12 text-center about-stat-panel">
+							<div class="stat-number">
+								<p>1 <span>in</span> 100</p>
+							</div>
 						</div>
 
+						<div class="col-sm-12 text-center about-paragraph-panel">
+							<div>
+								<p>Americans are victims of violent crime.</p>
+								<p>Let's help them get access to your services.</p>
+								<p>At our Title, we design and develop websites for survivor support agencies to better engage
+									with your clients help you improve the client's reporting and healing process.</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</section>
-		<!------------------------------------------------SECTION 3 - ABOUT---------------------------------------------->
-		<div class="about-background">
-			<div class="container mb-5">
-				<div class="row">
-					<div class="col-sm-12 text-center about-stat-panel">
-						<div class="stat-number">
-							<p>1 <span>in</span> 100</p>
-						</div>
-					</div>
-
-					<div class="col-sm-12 text-center about-paragraph-panel">
-						<div>
-							<p>Americans are victims of violent crime. Let's help them get access to your services.</p>
-							<p>At our Title, we design and develop websites for survivor support agencies to better engage
-								with their clients
-								and improve the client's reporting and healing process.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 
 
-		<!------------------------------------------------SECTION 4 - OUR SERVICES--------------------------------------->
-		<div class="services">
-			<div class="color-overlay">
-				<div class="container mb-5">
-					<div class="row">
+		<!------------------------------------------------SECTION 3 - OUR SERVICES--------------------------------------->
+		<section>
+			<div class="services">
+				<div class="color-overlay">
+					<div class="container mb-5">
+						<div class="row">
 
-						<div class="col-xs-12 col-md-5">
-							<h3 class="mt-5">Here is some text about our services</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque arcu eget quam
-								vulputate, ut dictum sem rutrum. Sed viverra gravida massa, ut luctus urna aliquam et. Sed
-								maximus felis non augue congue, ut accumsan mauris condimentum. Sed vitae lorem et quam commodo
-								interdum vitae id metus.</p>
-							<a href="#">Contact us --></a>
-						</div>
-
-						<div class="col-xs-12 col-md-6">
-							<div class="service-blocks">
-								<div class="services-block-1"></div>
-								<a href="#"></a>
-								<div class="service-heading"></div>
-								<h4>Service Heading 1</h4>
-								<div class="services-block-2"></div>
-								<a href="#"></a>
-								<div class="service-heading"></div>
-								<h4>Service Heading 2</h4>
-
-								<div class="services-block-3"></div>
-								<a href="#"></a>
-								<div class="service-heading"></div>
-								<h4>Service Heading 3</h4>
-
-								<div class="services-block-4"></div>
-								<a href="#"></a>
-								<div class="service-heading"></div>
-								<h4>Service Heading 4</h4>
+							<div class="col-xs-12 col-md-5">
+								<h3 class="mt-5">Here is some text about our services</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque arcu eget quam
+									vulputate, ut dictum sem rutrum. Sed viverra gravida massa, ut luctus urna aliquam et. Sed
+									maximus felis non augue congue, ut accumsan mauris condimentum. Sed vitae lorem et quam
+									commodo
+									interdum vitae id metus.</p>
+								<a href="#">Contact us --></a>
 							</div>
-						</div>
 
+							<div class="col-xs-12 col-md-6">
+								<div class="service-blocks">
+									<div class="services-block-1"></div>
+									<a href="#"></a>
+									<div class="service-heading"></div>
+									<h4>Service Heading 1</h4>
+									<div class="services-block-2"></div>
+									<a href="#"></a>
+									<div class="service-heading"></div>
+									<h4>Service Heading 2</h4>
+
+									<div class="services-block-3"></div>
+									<a href="#"></a>
+									<div class="service-heading"></div>
+									<h4>Service Heading 3</h4>
+
+									<div class="services-block-4"></div>
+									<a href="#"></a>
+									<div class="service-heading"></div>
+									<h4>Service Heading 4</h4>
+								</div>
+							</div>
+
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 
+		<!------------------------------------------------SECTION 4 - OUR PROCESS--------------------------------------->
 
 		<div class="row" style="height: 300px;">
 			<div class="col-4 text-center border">
@@ -140,6 +149,77 @@
 				</div>
 			</div>
 		</div>
+
+		<!------------------------------------------------SECTION 5 - PORTFOLIO------------------------------------------>
+
+
+		<!------------------------------------------------SECTION 6 - CONTACT FORM--------------------------------------->
+		<section class="container mb-4">
+			<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+			<p class="text-center w-responsive mx-auto mb-5">Ready to get started on your website design? Let's us know,
+				and we'll get back to you within 24 hours to discuss your project.</p>
+
+			<div class="row">
+				<div class="col-md-9 mb-md-0 mb-5">
+					<form id="contact-form" name="contact-form" action="#" method="POST">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="md-form mb-0">
+									<input type="text" id="name" name="name" class="form-control">
+									<label for="name" class="">Your name</label>
+								</div>
+							</div>
+
+							<div class="col-md-6">
+								<div class="md-form mb-0">
+									<input type="text" id="email" name="email" class="form-control">
+									<label for="email" class="">Your email</label>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-12">
+								<div class="md-form mb-0">
+									<input type="text" id="subject" name="subject" class="form-control">
+									<label for="subject" class="">Subject</label>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-12">
+								<div class="md-form">
+									<textarea type="text" id="message" name="message" rows="2"
+												 class="form-control md-textarea"></textarea>
+									<label for="message">Your message</label>
+								</div>
+							</div>
+						</div>
+					</form>
+
+					<div class="text-center text-md-left">
+						<button type="button" class="btn btn-info">Submit</button>
+					</div>
+					<div class="status"></div>
+				</div>
+
+				<div class="col-md-3 text-center">
+					<ul class="list-unstyled mb-0">
+						<li>
+							<p>Albuquerque, NM 87111, USA</p>
+						</li>
+						<li>
+							<p>000-000-0000</p>
+						</li>
+
+						<li>
+							<p>contact@marshabattee.com</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
 
 
 	</body>
