6d09cc4636c2911697b1044fa85fe36792f6689e
Marsha Battee <marsha@sanesuite.com>
2019-08-11 23:46:14 -0600

    added contact form background and "our process" text

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		<section>
 			<div class="container-fluid">
 				<nav class="navbar navbar-expand-md bg-transparent m-4">
-					<a class="navbar-brand text-white" href="#">Our Working Logo</a>
+					<a class="navbar-brand text-white" href="#">Our Logo</a>
 					<button class="navbar-toggler border bg-secondary" type="button" data-toggle="collapse"
 							  data-target="#collapsibleNavbar">
 						<span class="navbar-toggler-icon"></span>
@@ -46,7 +46,7 @@
 				<div class="container mx-5 my-5">
 
 					<div class="row banner-text">
-						<div class="col banner-title">Our Working Title</div>
+						<div class="col banner-title">Website Company Name</div>
 					</div>
 
 					<div class="row banner-text">
@@ -88,7 +88,7 @@
 		<section>
 			<div class="services">
 				<div class="color-overlay">
-					<div class="container mb-5">
+					<div class="container mt-5">
 						<div class="row">
 
 							<div class="col-xs-12 col-md-5">
@@ -106,21 +106,21 @@
 									<div class="services-block-1"></div>
 									<a href="#"></a>
 									<div class="service-heading"></div>
-									<h4>Service Heading 1</h4>
+									<h4>Website Development</h4>
 									<div class="services-block-2"></div>
 									<a href="#"></a>
 									<div class="service-heading"></div>
-									<h4>Service Heading 2</h4>
+									<h4>Interactive Design</h4>
 
 									<div class="services-block-3"></div>
 									<a href="#"></a>
 									<div class="service-heading"></div>
-									<h4>Service Heading 3</h4>
+									<h4>Research & Website Content Strategy</h4>
 
 									<div class="services-block-4"></div>
 									<a href="#"></a>
 									<div class="service-heading"></div>
-									<h4>Service Heading 4</h4>
+									<h4>Branding & Art Direction</h4>
 								</div>
 							</div>
 
@@ -131,96 +131,138 @@
 		</section>
 
 		<!------------------------------------------------SECTION 4 - OUR PROCESS--------------------------------------->
+		<section class="card-deck-background">
+			<div class="container">
+				<div class="card-deck">
+					<div class="card mt-5 bg-transparent border-0">
+						<img src="..." class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">DISCOVERY</h5>
+							<p class="card-text">This card has supporting text below as a natural lead-in to additional
+								content.</p>
+						</div>
+					</div>
+					<div class="card mt-5 bg-transparent border-0">
+						<img src="..." class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">RESEARCH & PLAN</h5>
+							<p class="card-text">This card has supporting text below as a natural lead-in to additional
+								content.</p>
+						</div>
+					</div>
+					<div class="card mt-5 bg-transparent border-0">
+						<img src="..." class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">WEBSITE DESIGN</h5>
+							<p class="card-text">This card has supporting text below as a natural lead-in to additional
+								content.</p>
+						</div>
+					</div>
+				</div>
 
-		<div class="row" style="height: 300px;">
-			<div class="col-4 text-center border">
 				<div>
-					<p>Website Development & Interactive Design</p>
+					<div class="card-deck mt-5">
+						<div class="card mb-5 bg-transparent border-0">
+							<img src="" class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">WEBSITE DEVELOPMENT</h5>
+								<p class="card-text">This card has supporting text below as a natural lead-in to additional
+									content.</p>
+							</div>
+						</div>
+						<div class="card mb-5 bg-transparent border-0">
+							<img src="..." class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">GO LIVE!</h5>
+								<p class="card-text">This card has supporting text below as a natural lead-in to additional
+									content.</p>
+							</div>
+						</div>
+						<div class="card mb-5 bg-transparent border-0">
+							<img src="..." class="card-img-top" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">MAINTENANCE & UPDATES</h5>
+								<p class="card-text">This card has supporting text below as a natural lead-in to additional
+									content.</p>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="col-4 text-center border">
-				<div>
-					<p>Research & Website Content Strategy </p>
-				</div>
-			</div>
-			<div class="col-4 text-center border">
-				<div>
-					<p>Branding & Art Direction</p>
-				</div>
-			</div>
-		</div>
-
+		</section>
 		<!------------------------------------------------SECTION 5 - PORTFOLIO------------------------------------------>
 
 
 		<!------------------------------------------------SECTION 6 - CONTACT FORM--------------------------------------->
-		<section class="container mb-4">
-			<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-			<p class="text-center w-responsive mx-auto mb-5">Ready to get started on your website design? Let's us know,
-				and we'll get back to you within 24 hours to discuss your project.</p>
+		<section class="footer">
+			<div class="banner-overlay3"></div>
+			<div class="banner-overlay4"></div>
+			<div class="container">
 
-			<div class="row">
-				<div class="col-md-9 mb-md-0 mb-5">
-					<form id="contact-form" name="contact-form" action="#" method="POST">
-						<div class="row">
-							<div class="col-md-6">
-								<div class="md-form mb-0">
-									<input type="text" id="name" name="name" class="form-control">
-									<label for="name" class="">Your name</label>
+				<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+				<p class="text-center w-responsive mx-auto mb-5">Ready to get started on your website design? Let's us know,
+					and we'll get back to you within 24 hours to discuss your project.</p>
+
+				<div class="row">
+					<div class="col-md-9 mb-md-0 mb-5">
+						<form id="contact-form" name="contact-form" action="#" method="POST">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="md-form mb-0">
+										<input type="text" id="name" name="name" class="form-control bg-transparent">
+										<label for="name" class="">Your name</label>
+									</div>
+								</div>
+
+								<div class="col-md-6">
+									<div class="md-form mb-0">
+										<input type="text" id="email" name="email" class="form-control bg-transparent">
+										<label for="email" class="">Your email</label>
+									</div>
 								</div>
 							</div>
 
-							<div class="col-md-6">
-								<div class="md-form mb-0">
-									<input type="text" id="email" name="email" class="form-control">
-									<label for="email" class="">Your email</label>
+							<div class="row">
+								<div class="col-md-12">
+									<div class="md-form mb-0">
+										<input type="text" id="subject" name="subject" class="form-control bg-transparent">
+										<label for="subject" class="">Subject</label>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div class="row">
-							<div class="col-md-12">
-								<div class="md-form mb-0">
-									<input type="text" id="subject" name="subject" class="form-control">
-									<label for="subject" class="">Subject</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="row">
-							<div class="col-md-12">
-								<div class="md-form">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="md-form">
 									<textarea type="text" id="message" name="message" rows="2"
-												 class="form-control md-textarea"></textarea>
-									<label for="message">Your message</label>
+												 class="form-control md-textarea bg-transparent"></textarea>
+										<label for="message">Your message</label>
+									</div>
 								</div>
 							</div>
+						</form>
+
+						<div class="text-center text-md-left">
+							<button type="button" class="btn btn-info">Submit</button>
 						</div>
-					</form>
-
-					<div class="text-center text-md-left">
-						<button type="button" class="btn btn-info">Submit</button>
+						<div class="status"></div>
 					</div>
-					<div class="status"></div>
-				</div>
 
-				<div class="col-md-3 text-center">
-					<ul class="list-unstyled mb-0">
-						<li>
-							<p>Albuquerque, NM 87111, USA</p>
-						</li>
-						<li>
-							<p>000-000-0000</p>
-						</li>
-
-						<li>
-							<p>contact@marshabattee.com</p>
-						</li>
-					</ul>
+					<div class="col-md-3 text-center">
+						<ul class="list-unstyled mb-0">
+							<li>
+								<p>Albuquerque, NM 87111, USA</p>
+							</li>
+							<li>
+								<p>000-000-0000</p>
+							</li>
+							<li>
+								<p>contact@marshabattee.com</p>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</section>
-
 
 	</body>
 </html>
